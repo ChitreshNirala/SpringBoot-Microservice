1c9e6845bf65d22e2ca2c2fd8eb6692be8302ee3
Chitresh Nirala <chitresh.nirala@gmail.com>
2026-02-11 21:02:54 +0530

    Calling ReduceQuantity API from placeOrder Service using Feign Client

--- OrderService/src/main/java/com/dailycodebuffer/OrderService/services/OrderServiceImpl.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/services/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.OrderService.services;
 
 import com.dailycodebuffer.OrderService.entity.Order;
+import com.dailycodebuffer.OrderService.external.client.ProductService;
 import com.dailycodebuffer.OrderService.model.OrderRequest;
 import com.dailycodebuffer.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -16,6 +17,9 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductService productService;
+
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
@@ -23,6 +27,10 @@ public class OrderServiceImpl implements OrderService {
         //product service - block the products (reduce the quantity)
         //payment service - make payment --success-- complete, else cancel the order
         log.info("Placing order request: {}", orderRequest);
+
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+
+        log.info("Creating order with status CREATED");
         Order order = Order.builder()
                 .amount(orderRequest.getTotalAmount())
                 .productId(orderRequest.getProductId())
