170b87132d884da7a7e151f7c0ad5394abcd7b7b
Chitresh Nirala <chitresh.nirala@gmail.com>
2026-02-11 21:46:32 +0530

    Implementing ErrorDecoder to handle Exceptions

---
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/external/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.dailycodebuffer.OrderService.external.exception;
+
+
+import com.dailycodebuffer.OrderService.external.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .errorCode(ex.getErrorCode())
+                .errorMessage(ex.getMessage())
+                .build(), HttpStatus.valueOf(ex.getStatus()));
+    }
+}
