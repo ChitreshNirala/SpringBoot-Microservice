60433527ef7fd975f671b47ac020f4376e908e9a
Chitresh Nirala <chitresh.nirala@gmail.com>
2026-02-12 18:46:01 +0530

    Calling doPayment(PaymentService) API with Feign Client from placeOrder(Order Service)

---
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/external/request/PaymentRequest.java
@@ -0,0 +1,20 @@
+package com.dailycodebuffer.OrderService.external.request;
+
+import com.dailycodebuffer.OrderService.model.PaymentMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentRequest {
+
+    private long orderId;
+    private long amount;
+    private PaymentMode paymentMode;
+    private String referenceNumber;
+
+}
