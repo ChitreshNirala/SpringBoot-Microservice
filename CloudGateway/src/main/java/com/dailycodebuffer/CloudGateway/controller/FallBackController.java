63b1bbc004235d9fcf1d7a4cdead3b01d627d9e0
Chitresh Nirala <chitresh.nirala@gmail.com>
2026-02-16 23:03:20 +0530

    Implementing Circuit Breaker in Order Service and rate Limiter

--- CloudGateway/src/main/java/com/dailycodebuffer/CloudGateway/controller/FallBackController.java
+++ CloudGateway/src/main/java/com/dailycodebuffer/CloudGateway/controller/FallBackController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FallBackController {
 
-    @GetMapping("/orderServiceFallBack")
+    @GetMapping("/orderServiceFallback")
     public String orderServiceFallBackMethod() {
         return "Order Service is down.";
     }
--- OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.dailycodebuffer.OrderService.external.client;
 
+import com.dailycodebuffer.OrderService.external.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/products")
 public interface ProductService {    
 
@@ -14,4 +17,8 @@ public interface ProductService {
                 @PathVariable("id") long productId,
                 @RequestParam("quantity") long quantity
         );
+
+        default void fallback(Exception e) {
+                throw new CustomException("Product service is down","UNAVAILABLE",500);
+        }
 }
